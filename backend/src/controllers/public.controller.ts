0f34c64f26748a1946110d060750b64978b1db9f
shaktissr19 <shakti.ssr19@gmail.com>
2026-08-22 16:40:25 +0530

    Expose public landing overview endpoint

---
+++ backend/src/controllers/public.controller.ts
@@ -0,0 +1,15 @@
+import type { NextFunction, Request, Response } from 'express';
+import * as publicOverviewService from '../services/publicOverview.service';
+import * as R from '../utils/response';
+
+export async function overview(
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<Response | void> {
+  try {
+    return R.ok(res, await publicOverviewService.getPublicOverview());
+  } catch (error: unknown) {
+    next(error);
+  }
+}
