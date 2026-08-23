a62b78ed5fe96619c01aafb8c9416e884494faaa
shaktissr19 <shakti.ssr19@gmail.com>
2026-08-23 12:03:47 +0530

    Add public Learning practice route

---
+++ frontend/src/app/learn/practice/[slug]/page.tsx
@@ -0,0 +1,5 @@
+import PublicPracticeAssessmentPage from '@/components/public/PublicPracticeAssessmentPage';
+
+export default function LearningPracticePage() {
+  return <PublicPracticeAssessmentPage />;
+}
