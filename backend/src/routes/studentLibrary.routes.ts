cee9978df5b4fdb155a22b2e0478c00f1440330c
shaktissr19 <shakti.ssr19@gmail.com>
2026-09-04 16:12:25 +0530

    chore(library): remove student physical library routes

--- backend/src/routes/studentLibrary.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as ctrl from '../controllers/library.controller';
-import { authenticate, authorize } from '../middleware/auth.middleware';
-const router=Router();
-router.use(authenticate);
-router.use(authorize('STUDENT'));
-router.get('/loans',ctrl.studentLoans);
-export = router;
